74e49157a6acbc26d7af88b72c42b4702114f16e
niko wahyu fitrianto <nikonabilah@gmail.com>
2023-07-21 12:03:07 +0700

    Add project files.

---
+++ PalindromTest/Program.cs
@@ -0,0 +1,9 @@
+﻿while (true)
+{
+    Console.WriteLine("masukan kata");
+    Console.Write("inputan: ");
+    string kataKata = Console.ReadLine();
+    bool isPalindom = kataKata.SequenceEqual(kataKata.Reverse());
+    Console.WriteLine(string.Format("apakah dia adalah palindom ? {0}", isPalindom));
+    Console.WriteLine("====================================");
+}
