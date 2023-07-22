8f2e95f9340ac9e0e0c013adb0174df1937551cc
niko wahyu fitrianto <nikonabilah@gmail.com>
2023-07-23 05:40:30 +0700

    [#] inputan semua dibuat lowercase

--- PalindromTest/Program.cs
+++ PalindromTest/Program.cs
@@ -1,9 +1,16 @@
-﻿while (true)
+﻿internal class Program
 {
-    Console.WriteLine("masukan kata");
-    Console.Write("inputan: ");
-    string kataKata = Console.ReadLine();
-    bool isPalindom = kataKata.SequenceEqual(kataKata.Reverse());
-    Console.WriteLine(string.Format("apakah dia adalah palindom ? {0}", isPalindom));
-    Console.WriteLine("====================================");
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("masukan kata");
+            Console.Write("inputan: ");
+            string kataKata = Console.ReadLine();
+            kataKata = kataKata.ToLower();
+            bool isPalindom = kataKata.SequenceEqual(kataKata.Reverse());
+            Console.WriteLine(string.Format("apakah dia adalah palindom ? {0}", isPalindom));
+            Console.WriteLine("====================================");
+        }
+    }
 }
